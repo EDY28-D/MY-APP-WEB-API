02a48c24bea865e77c5ce1cdce9f83cf25ee3fa8
EDY28 <junior.matias@unas.edu.pe>
2024-04-30 08:23:12 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MY_APP_WEB_API.Controllers.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MY_APP_WEB_API.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+   
+        public ActionResult<IEnumerable<Persona>> listarCliente() 
+
+        {
+            var clientes = new List<Persona>
+            {
+                new Persona
+                {
+                    id = "1",
+                    correo = "google@gmail.com",
+                    edad = "22",
+                    nombre = "Matias Bardales"
+                },
+                new Persona
+                {
+                    id = "2",
+                    correo = "jean@gmail.com",
+                    edad = "23",
+                    nombre = "jean Bardales"
+                }
+            };
+            return Ok(clientes);
+        }
+
+        [HttpGet]
+        [Route("listarxid")]
+        public ActionResult<Persona> listarClientexid(int codigo)
+        {
+            // Obtener el cliente de la base de datos según el código proporcionado
+            var cliente = new Persona
+            {
+                id = codigo.ToString(),
+                correo = "jean@gmail.com",
+                edad = "23",
+                nombre = "jean Bardales"
+            };
+            return Ok(cliente);
+        }
+
+        [HttpPost]
+        [Route("guardar")]
+        public ActionResult<dynamic> guardarCliente(Persona cliente)
+        {
+            cliente.id = "3";
+            return Ok(new
+            {
+                success = true,
+                message = "Cliente agregado",
+                result = cliente
+            });
+        }
+
+        [HttpPost]
+        [Route("eliminar")]
+        public ActionResult<dynamic> eliminarCliente(Persona cliente)
+        {
+            string token = Request.Headers["Authorization"].FirstOrDefault();
+
+            // Eliminar el cliente de la base de datos
+            if (token != "marco123.")
+            {
+                return BadRequest(); // Devuelve un HTTP 400 sin ningún contenido adicional
+            }
+            else
+            {
+                // Procede con la eliminación del cliente y devuelve una respuesta exitosa
+                return Ok(new
+                {
+                    success = true,
+                    message = "Cliente eliminado",
+                    result = cliente
+                });
+            }
+        }
+    }
+}
---
+++ Controllers/Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace MY_APP_WEB_API.Controllers.Models
+{
+    public class  Persona
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string edad { get; set;}
+        public string correo { get; set;}
+    }
+}
